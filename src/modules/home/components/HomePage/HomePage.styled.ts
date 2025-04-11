1bffbb3fde0d1eb62363f4b30081a0690353db18
Limhill <a.hulmurzin@gmail.com>
2025-04-11 09:48:27 +0500

    refactor: add styled props for Card

--- src/modules/home/components/HomePage/HomePage.styled.ts
+++ src/modules/home/components/HomePage/HomePage.styled.ts
@@ -2,27 +2,22 @@ import styled from 'styled-components';
 import { T } from '@admiral-ds/react-ui';
 import { LIGHT_THEME } from '@admiral-ds/react-ui';
 
-export const Card = styled.div`
+export const Card = styled.div<{ disabled: boolean }>`
     border: 1px solid ${LIGHT_THEME.color['Neutral/Neutral 20']};
     border-radius: 10px;
     padding: 16px;
     background-color: ${LIGHT_THEME.color['Neutral/Neutral 00']};
     color: ${LIGHT_THEME.color['Neutral/Neutral 00']};
-    cursor: pointer;
     height: 90px;
-
-    &[data-disabled="true"] {
-        cursor: not-allowed; //Почему то не отображается курсор, как указано.. (вроде бы не ошибся с условиями)
-        pointer-events: none;
-        opacity: 0.5;
-    }
+    cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
+    opacity: ${({ disabled }) => (disabled ? '0.5' : '1')};
 `;
 
 export const PathPage = styled(T)`
     margin: 20px;
     font-size: 18px;
     font-weight: 400;
-`
+`;
 
 export const ContainerCards = styled.div`
     padding: 0 16px;
--- src/modules/home/components/HomePage/HomePage.tsx
+++ src/modules/home/components/HomePage/HomePage.tsx
@@ -13,7 +13,7 @@ export const HomePage = () => {
             </PathPage>
             <ContainerCards>
                 {cardsData.map(({ id, title, path }) => (
-                    <Card key={id} title={title} data-disabled={!path} onClick={() => handleCardClick(path)}>
+                    <Card key={id} title={title} onClick={() => handleCardClick(path)} disabled={!path}>
                         <T font="Body/Body 2 Long" as="h1" color="Primary/Primary 60 Main">
                             {title}
                         </T>
